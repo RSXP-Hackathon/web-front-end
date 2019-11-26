f7672daeb08967a8167b86ac948dac08a64ab24b
Henry Franklin <henrymais@gmail.com>
2019-11-26 18:00:55 -0300

    feat: user data de acordo com o perfil

--- src/layouts/Admin.jsx
+++ src/layouts/Admin.jsx
@@ -25,6 +25,7 @@ import AdminFooter from "components/Footers/AdminFooter.jsx";
 import Sidebar from "components/Sidebar/Sidebar.jsx";
 
 import routes from "routes.js";
+import users from "users.js";
 
 class Admin extends React.Component {
   componentDidUpdate(e) {
@@ -48,8 +49,9 @@ class Admin extends React.Component {
       }
     });
   };
-  getBrandText = path => {
-    for (let i = 0; i < routes.length; i++) {
+  getUserData = (group) => {
+    return users[group];
+  };
   getBrandText = (path, group) => {
     for (let i = 0; i < routes[group].length; i++) {
       if (
@@ -77,6 +79,7 @@ class Admin extends React.Component {
         <div className="main-content" ref="mainContent">
           <AdminNavbar
             {...this.props}
+            usuarioLogado={this.getUserData(this.props.routeGroup)}
             brandText={this.getBrandText(this.props.location.pathname,  this.props.routeGroup)}
           />
           <Switch>{this.getRoutes(routes[this.props.routeGroup])}</Switch>
