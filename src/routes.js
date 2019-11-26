df0d536e1ca528944d3faa16c7c7ab92199f2a5b
Henry Franklin <henrymais@gmail.com>
2019-11-26 18:00:55 -0300

    feat: user data de acordo com o perfil

--- src/routes.js
+++ src/routes.js
@@ -31,7 +31,7 @@ var routes = {
     {
       path: "/empresa",
       group: 'empresa',
-      name: "Empresa",
+      name: "Início",
       icon: "ni ni-tv-2 text-primary",
       component: Empresa,
       layout: "/admin"
---
+++ src/users.js
@@ -0,0 +1,14 @@
+export default {
+  empresa: {
+    name: 'Empresa X',
+    avatar: require('assets/img/theme/team-2-800x800.jpg'),
+  },
+  mentor: {
+    name: 'Jessica Jones',
+    avatar: require('assets/img/theme/team-4-800x800.jpg'),
+  },
+  aluno: {
+    name: 'Zezinho',
+    avatar: require('assets/img/theme/team-1-800x800.jpg'),
+  }
+};
